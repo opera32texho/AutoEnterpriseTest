97ef5e2ab792f46e6582ef2a92cf954fe30f0098
Vitalii <wcrwcr1993@gmail.com>
2020-09-27 00:28:29 +0300

    CreateWebSite

---
+++ project/js/main.js
@@ -0,0 +1,15 @@
+function openCity(evt, stationName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab-links");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(stationName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+//   cityName
